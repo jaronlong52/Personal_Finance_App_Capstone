b61edc6f9f8065a6b769d05e118deb7d35def341
Jaron <longjm3@appstate.edu>
2024-09-05 18:42:03 -0400

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// changing this as a test
+
 function App() {
   const [count, setCount] = useState(0)
 
