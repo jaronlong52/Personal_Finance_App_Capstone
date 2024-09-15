40c3a3a4670ed185959b64fb8bccf40bc9b22765
jaronlong52 <longjm3@appstate.edu>
2024-09-15 13:40:25 -0400

    Fixed login page appearance but doesn't work yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './components/home'
-import Login from './components/login'
+import Login from './components/login/login'
 import './App.css'
 import { useEffect, useState } from 'react'
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false)
-  const [email, setEmail] = useState('')
-
-  useEffect(() => {
-    // Fetch the user email and token from local storage
-    const user = JSON.parse(localStorage.getItem('user'))
-  
-    // If the token/email does not exist, mark the user as logged out
-    if (!user || !user.token) {
-      setLoggedIn(false)
-      return
-    }
-  
-    // If the token exists, verify it with the auth server to see if it is valid
-    fetch('http://localhost:3080/verify', {
-      method: 'POST',
-      headers: {
-        'jwt-token': user.token,
-      },
-    })
-      .then((r) => r.json())
-      .then((r) => {
-        setLoggedIn('success' === r.message)
-        setEmail(user.email || '')
-      })
-  }, [])
-
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={<Home email={email} loggedIn={loggedIn} setLoggedIn={setLoggedIn} />}
-          />
-          <Route path="/login" element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />} />
-        </Routes>
-      </BrowserRouter>
+    <div>
+      <Login/>
     </div>
   )
 }
 
 export default App
-
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// // changing this as a test
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
---
+++ src/components/login/login.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import './login.css'
+import userData from '../../userData.json'
+
+import user_icon from '../../assets/user.png'
+import password_icon from '../../assets/padlock.png'
+
+const Login = () => {
+
+  const [action, setAction] = useState("Login");
+
+  return (
+    <div className='container'>
+      <div className='header'>
+        <div className='text'>Qwik Finance</div>
+        <div className='underline'></div>
+      </div>
+      <div className='inputs'>
+        {action==="Login"?<div></div>:<div className='input'>
+          <img src={user_icon} alt="" />
+          <input type="name" placeholder="Name"/>
+        </div>}
+        <div className='input'>
+          <img src={user_icon} alt="" />
+          <input type="username" placeholder="Username"/>
+        </div>
+        <div className='input'>
+          <img src={password_icon} alt="" />
+          <input type="password" placeholder="Password"/>
+        </div>
+      </div>
+      {action==="Sign Up"?<div></div>:<div className="forgot-password">Forget Password? <span>Click Here!</span></div>}
+      <div className="submit-container">
+        <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+        <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
+      </div>
+    </div>
+  )
+}
+
+
+export default Login
