87e611ac2594a07ba06b05f457a680d09b119e44
Jaron <longjm3@appstate.edu>
2024-10-04 12:22:55 -0400

    added input validation and error labels to display errors

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -22,6 +22,10 @@ const Login = () => {
   // all usernames in database
   const [usernames, setUsernames] = useState([]);
 
+  // Hold error messages for login and sign up input
+  const [errorUsername, setErrorUsername] = useState('');
+  const [errorPassword, setErrorPassword] = useState('');
+
   // gets all usernames from database and stores it in usernames
   useEffect(() => {
     axios.get('http://localhost:8081/users')
@@ -31,20 +35,66 @@ const Login = () => {
     });
   }, []);
 
-  // alerts user if the entered username is already in database
-  const usernameTakenAlert = () => {
-    alert("This username is already associated with an account!");
-  };
+  // Validate user input and set error messages
+  const validateInput = () => {
+    // set error messages to empty
+    setErrorUsername('');
+    setErrorPassword('');
+
+    // check if fields are empty
+    if ('' === username) {
+      setErrorUsername('Please enter your username');
+      return
+    }
+
+    if ('' === password) {
+      setErrorPassword('Please enter your password');
+      return
+    }
+
+    // check if fields are valid length
+    if (username.length > 19) {
+      setErrorUsername('Username is too long');
+      return
+    }
+
+    if (username.length < 1) {
+      setErrorUsername('Username is too short');
+      return
+    }
+
+    if (password.length > 19) {
+      setErrorPassword('Password is too long');
+      return
+    }
+
+    if (password.length < 6) {
+      setErrorUsername('Password must be at least 6 characters');
+      return
+    }
+
+    // for login, check if username does not exist
+    if (action === 'Login') {
+      if (!usernames.some(obj => obj.username === username)) {
+        setErrorUsername('Username does not exist');
+        return
+      }
+    }
+
+    // for sign up, check if username is already in use
+    if (action === 'Sign Up') {
+      if (usernames.some(obj => obj.username === username)) {
+        setErrorUsername('Username already in use');
+        return
+      }
+    }
+  }
 
   // test user input and, if valid, enter data into database
   const register = () => {
 
-    // test if the entered username is already being used
-    // don't enter info into database if it already exists (purpose of the return)
-    if (usernames.some(obj => obj.username === username)) {
-      usernameTakenAlert();
-      return
-    }
+    // validate user input
+    validateInput();
 
     // enter user input into database
     axios.post('http://localhost:8081/register', {
@@ -56,10 +106,15 @@ const Login = () => {
     });
   };
 
+  // TO DO
+  // validate and authenticate user input 
+  // navigate to dashboard upon login so pull dashboard files into branch
+
   // test user input and, if valid, let user access data
   const login = () => {
 
-    // place to validate login info
+    // validate user input
+    validateInput();
 
     // see if user input info is in database and set loginStatus based on that
     // could probably use a .get instead of .post but not really any reason to that I know of
@@ -73,6 +128,10 @@ const Login = () => {
         setLoginStatus('true')
       }
     });
+
+    // if (loginStatus === 'false') {
+
+    // }
   };
 
   return (
@@ -88,12 +147,14 @@ const Login = () => {
           </div>}
           <div className='input'>
             <img src={user_icon} alt="" />
-            <input type="username" value={username} onChange={e => setUsername(e.target.value)} placeholder="Username"/>
+            <input id="username" type="username" value={username} onChange={e => setUsername(e.target.value)} placeholder="Username"/>
           </div>
+          <label className="error-label" htmlFor="username">{errorUsername}</label>
           <div className='input'>
             <img src={password_icon} alt="" />
-            <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password"/>
+            <input id="password" type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password"/>
           </div>
+          <label className="error-label" htmlFor="password">{errorPassword}</label>
         </div>
         {action==="Sign Up"?<div></div>:<div className="forgot-password">Forget Password? <span>Click Here!</span></div>}
         <div className="info-submit-container">
