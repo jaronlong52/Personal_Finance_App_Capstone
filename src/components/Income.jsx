36479a17c7c4bc92f34d59b704c85cc688d1e988
Jaron <longjm3@appstate.edu>
2024-10-23 14:15:57 -0400

    added input submission for income data and altered appearance

--- src/components/Income.jsx
+++ src/components/Income.jsx
@@ -3,13 +3,19 @@ import './Income.css';
 
 function Income() {
     const [data, setData] = useState([
-        { Date: '10-04-24', Amount: 1600, Comments: 'Weekly salary'},
-        { Date: '10-09-24', Amount: 400, Comments: 'Side hustle'},
-        { Date: '10-11-24', Amount: 1600, Comments: 'Weekly salary'},
+        { Date: '10/04/24', Amount: 1600, Comments: 'Weekly salary'},
+        { Date: '10/09/24', Amount: 400, Comments: 'Side hustle'},
+        { Date: '10/11/24', Amount: 1600, Comments: 'Weekly salary'},
     ]);
 
     return (
         <div className="container">
+            <div className="inputs">
+                <input className="input" type="date" placeholder='date' />
+                <input className="input" type="amount" placeholder='Amount' />
+                <input className="comments" type="comments" placeholder='Comments' />
+                <button className='income-submit'>Submit</button>
+            </div>
             <table className="income-table">
                     <thead>
                         <tr>
