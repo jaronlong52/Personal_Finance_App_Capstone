479cfe6b50495222e185508a12b330a31adbb7c3
Jaron <longjm3@appstate.edu>
2024-10-23 13:44:58 -0400

    added income records table with example data

--- src/components/Income.jsx
+++ src/components/Income.jsx
@@ -1,9 +1,33 @@
 import React, { useState } from 'react';
+import './Income.css';
 
 function Income() {
+    const [data, setData] = useState([
+        { Date: '10-04-24', Amount: 1600, Comments: 'Weekly salary'},
+        { Date: '10-09-24', Amount: 400, Comments: 'Side hustle'},
+        { Date: '10-11-24', Amount: 1600, Comments: 'Weekly salary'},
+    ]);
+
     return (
         <div className="container">
-        <h1>This is the cool income stuff</h1>
+            <table className="income-table">
+                    <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Amount ($)</th>
+                            <th>Comments</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map((item, index) => (
+                            <tr key={index}>
+                                <td>{item.Date}</td>
+                                <td>{item.Amount}</td>
+                                <td>{item.Comments}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
         </div>
     )
 }
